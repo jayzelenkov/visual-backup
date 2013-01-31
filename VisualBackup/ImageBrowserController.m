1e137d11cf26383cba75a80fce8992ecf7225346
Jevgeni Zelenkov <jzelenkov@me.com>
2013-01-31 03:00:17 +0100

    only images in ~/Pictures are loaded when app starts

--- VisualBackup/ImageBrowserController.m
+++ VisualBackup/ImageBrowserController.m
@@ -35,35 +35,40 @@
     
     _images = [[NSMutableArray alloc] init];
     _importedImages = [[NSMutableArray alloc] init];
-    
-    
+
+
     //allow reordering, animations et set draggind destination delegate
     [_imageBrowser setAllowsReordering:YES];
     [_imageBrowser setAnimates:YES];
     [_imageBrowser setDraggingDestinationDelegate:self];
+    
+    // load images on load
+    [self addImageButtonClicked:nil];
 }
-
-
-
-
 
 - (IBAction)addImageButtonClicked:(id)sender
 {
-    // load images here
-    
-    // [_imageBrowser reloadData];
-    
-    
-    NSArray *urls = openFiles();
-    
-    if (!urls)
+    NSString *picsDir = [NSHomeDirectory() stringByAppendingPathComponent:  @"Pictures"];
+    NSArray *filepaths = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:picsDir error:nil];
+    NSMutableArray *images = [NSMutableArray arrayWithCapacity: 0];
+
+    NSInteger i, n;
+    n = [filepaths count];
+
+    [_images removeAllObjects];
+
+    for ( i= 0; i < n; i++)
     {
-        NSLog(@"No files selected, return...");
-        return;
+        NSString *fullPath = [NSString stringWithFormat:@"%@/%@", picsDir, [filepaths objectAtIndex:i]];
+        NSURL *url = [NSURL fileURLWithPath:fullPath];
+        if(url != nil)
+        {
+            [images addObject:url];
+        }
     }
-	
-	/* launch import in an independent thread */
-    [NSThread detachNewThreadSelector:@selector(addImagesWithPaths:) toTarget:self withObject:urls];
+
+    /* launch import in an independent thread */
+    [NSThread detachNewThreadSelector:@selector(addImagesWithPaths:) toTarget:self withObject:images];
 
 }
 
@@ -77,13 +82,6 @@
 {
     return [_images objectAtIndex:index];
 }
-
-
-
-
-
-
-// BLAH!
 
 
 /* entry point for reloading image-browser's data and setNeedsDisplay */
@@ -110,79 +108,26 @@
     [_importedImages addObject:p];
 }
 
-
-
-
-- (void)addImagesWithPath:(NSString *)path recursive:(BOOL)recursive
-{
-    NSInteger i, n;
-    BOOL dir;
-    
-    [[NSFileManager defaultManager] fileExistsAtPath:path isDirectory:&dir];
-    
-    if (dir)
-    {
-        NSArray *content = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:path error:nil];
-        
-        n = [content count];
-        
-		// parse the directory content
-        for (i=0; i<n; i++)
-        {
-            if (recursive)
-                [self addImagesWithPath:[path stringByAppendingPathComponent:[content objectAtIndex:i]] recursive:YES];
-            else
-                [self addAnImageWithPath:[path stringByAppendingPathComponent:[content objectAtIndex:i]]];
-        }
-    }
-    else
-    {
-        [self addAnImageWithPath:path];
-    }
-}
-
 /* performed in an independant thread, parse all paths in "paths" and add these paths in our temporary array */
 - (void)addImagesWithPaths:(NSArray *)urls
 {
+    NSArray *imageTypes = [NSImage imageTypes];
     NSInteger i, n;
-        
+
     n = [urls count];
     for ( i= 0; i < n; i++)
     {
         NSURL *url = [urls objectAtIndex:i];
-        [self addImagesWithPath:[url path] recursive:NO];
+        
+        NSString *resType;
+        [url getResourceValue:&resType forKey:NSURLTypeIdentifierKey error:nil];
+        if([imageTypes containsObject:resType]) {
+            [self addAnImageWithPath:[url path]];
+        }
     }
     
 	/* update the datasource in the main thread */
     [self performSelectorOnMainThread:@selector(updateDatasource) withObject:nil waitUntilDone:YES];
-    
 }
-
-
-
-
-
-
-// openFiles is a simple C function that opens an NSOpenPanel and return an array of URLs
-static NSArray *openFiles()
-{
-    NSOpenPanel *panel;
-    
-    panel = [NSOpenPanel openPanel];
-    [panel setFloatingPanel:YES];
-    [panel setCanChooseDirectories:YES];
-    [panel setCanChooseFiles:YES];
-	NSInteger i = [panel runModal];
-	if (i == NSOKButton)
-    {
-		return [panel URLs];
-    }
-    
-    return nil;
-}
-
-
-
-
 
 @end
